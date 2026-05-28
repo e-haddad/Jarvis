6db45d6fc32563ce44efbe6eee801421637f6cad
Edward Haddad <edwardhaddad03@gmail.com>
2026-05-28 02:08:51 -0400

    Jarvis: update _read_full_fs.py

--- _read_full_fs.py
+++ _read_full_fs.py
@@ -1,5 +1,4 @@
 with open("filesystem.py", "r") as f:
     content = f.read()
-# Print from restore_backup onward
-idx = content.find("def restore_backup")
+idx = content.find("def move_file")
 print(content[idx:])
